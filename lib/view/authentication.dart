87a8141b08b1f9ecde2c28eb6d36e11cdb2b966a
Temurbek Sabirov <prodota150@gmail.com>
2020-10-20 13:38:48 -0400

    Style the register page

--- lib/view/authentication.dart
+++ lib/view/authentication.dart
@@ -22,34 +22,48 @@ class _AuthenticationState extends State<Authentication> {
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(color: Colors.blueAccent),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextFormField(
-              controller: _emailField,
-              decoration: InputDecoration(
-                hintText: "Enter Email",
-                hintStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                labelText: "Email",
-                labelStyle: TextStyle(
-                  color: Colors.white,
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _emailField,
+                decoration: InputDecoration(
+                  hintText: "Enter Email",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Email",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-            TextFormField(
-              controller: _passwordField,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Enter Password",
-                hintStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                labelText: "Password",
-                labelStyle: TextStyle(
-                  color: Colors.white,
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 35,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _passwordField,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 35,
             ),
             Container(
               width: MediaQuery.of(context).size.width / 1.4,
@@ -75,6 +89,9 @@ class _AuthenticationState extends State<Authentication> {
                 },
                 child: Text("Register"),
               ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 35,
             ),
             Container(
               width: MediaQuery.of(context).size.width / 1.4,
